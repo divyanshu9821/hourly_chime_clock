0a5037b6f5b43e70f7d7f45769d6562652ae9acb
divyanshu9821 <singhaldivyanshu2001@gmail.com>
2022-11-19 11:36:03 +0530

    add halts to stop execution of program when not in need

--- test.py
+++ test.py
@@ -3,11 +3,19 @@ from playsound import playsound
 
 switch = bool(input("Enter 1 for ON : "))
 
+tm = int(time.strftime("%M"))
+
+# to halt the program
+if(tm<30 and tm>=0):
+    time.sleep(29-tm)
+elif(tm<59 and tm>=30):
+    time.sleep(59-tm)
+
 while(switch):
-    curr_min = int(time.strftime("%M")) # get current minute reading
+    curr_min = int(time.strftime("%M")) # get current minute reading    
     
     # at every hour
-    if(curr_min==51):
+    if(curr_min==0):
         i = int(time.strftime("%H")) # get current hour reading
     
         # play sound according to hours
@@ -15,7 +23,9 @@ while(switch):
             playsound('dong.wav')
             i = i-1
     
-        time.sleep(60) # halt for 60 seconds
+        time.sleep(29-curr_min) #halt
+    
     # at every half hour
     elif(curr_min==30):
-        playsound('dong.wav')    
+        playsound('dong.wav')
+        time.sleep(59-curr_min) #halt   
