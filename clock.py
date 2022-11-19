c126d98495c379d71b6ea0c7ca83424a81af9164
divyanshu9821 <singhaldivyanshu2001@gmail.com>
2022-11-19 16:20:09 +0530

    break down into functions

--- clock.py
+++ clock.py
@@ -1,45 +1,77 @@
+import os
 import time
 from playsound import playsound
 
-print("------Pendulum clock------\n")
+#standalone function 
+def title_currTime():
+    os.system('cls')
+    print("------Pendulum clock------\n")
+    print("Time: "+time.strftime("%I : %M : %S")+"\n")
 
-print("Time: "+time.strftime("%I : %M : %S")+"\n")
+#standalone function
+def tick_tock():
+    time.sleep(1)
+    print("tick tock tick ...\n")
 
-time.sleep(2)
+def init():
+   
+    title_currTime()
 
-print("tick tock tick ...\n")
+    tick_tock()
 
-time.sleep(2)
+    time.sleep(1)
+    print("Minimize the window for your convinence and forget :)\n")
 
-print("Minimize the window for your convinence and forget :)\n")
+    time.sleep(2)
 
-time.sleep(2)
+    tick_tock()
 
-print("tick tock tick ...")
+#standalone function
+def init_halt():
+    minute = int(time.strftime("%M"))
 
-tm = int(time.strftime("%M"))
+    # to halt the program, it helps to reduce the cpu usage
+    if(minute<29 and minute>0):
+        time.sleep((29-minute)*60)
 
-# to halt the program
-if(tm<29 and tm>=0):
-    time.sleep((29-tm)*60)
-elif(tm<58 and tm>=30):
-    time.sleep((59-tm)*60)
+    elif(minute<58 and minute>30):
+        time.sleep((59-minute)*60)        
 
-while(True):
-    curr_min = int(time.strftime("%M")) # get current minute reading    
+if __name__ == "__main__" :
     
-    # at every hour
-    if(curr_min==0):
-        i = int(time.strftime("%I")) # get current hour reading
-    
-        # play sound according to hours
-        while(i): 
-            playsound('hours.wav')
-            i = i-1
-    
-        time.sleep(1680) #halt
-    
-    # at every half hour
-    elif(curr_min==30):
-        playsound('half.wav')
-        time.sleep(1680) #halt   
+    init()
+
+    init_halt()
+
+    while(True):
+        curr_min = int(time.strftime("%M")) # get current minute reading 
+
+        # at every hour
+        if(curr_min==0):
+            
+            
+            title_currTime()
+
+            i = int(time.strftime("%I")) # get current hour reading
+        
+            # play sound according to hours
+            while(i): 
+                print("Ring")
+                playsound('hours.wav')
+                i = i-1
+            
+            tick_tock()   
+
+            time.sleep(1680) #halt
+        
+        # at every half hour
+        elif(curr_min==30):
+
+            title_currTime()
+
+            print("Ring")
+            playsound('half.wav')
+
+            tick_tock()
+
+            time.sleep(1680) #halt   
